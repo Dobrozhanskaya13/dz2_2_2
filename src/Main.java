f8694761ccceeb0c8a575e30d990eeb55c485eef
dps <dobro.polya@yandex.ru>
2023-01-19 20:18:00 +0300

    Dz3 commit2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        //Уважаемый проверяющий, исправляю свою программу
-        //По условию задачи, не было сказано каким образом производить инициализацию переменных.
-        // Если я это где-то пропустила, прошу указать явно. С уважением, Доброжанская П.С.
-        int mile = 0;// мили
-        int bonus = 20;//рублей для одной бонусной мили
-        double ticket_price = 12334.90; //стоимость билета
-        mile = (int) ticket_price / bonus;
-        System.out.println("Стоимость билета " + ticket_price);
-        System.out.println("Количество бонусных миль " + mile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 12334;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
